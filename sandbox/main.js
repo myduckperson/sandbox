5dd5307ad147bc375b929f1478c2f6e3095f5897
myduckperson <illarionov0405@gmail.com>
2022-02-06 15:43:33 +0200

    starting to play deeper

--- sandbox/main.js
+++ sandbox/main.js
@@ -1,3 +1,4 @@
+"strict mode"
 const dating = {
     data(){
         return {
@@ -6,9 +7,44 @@ const dating = {
     },
     mounted() {
         setInterval(() => {
-          this.theDate = new Date
+        this.theDate = new Date
         }, 1000)
-      }
+    }
 };
+const handleTheEvent = {
+    data(){
+        return {
+            message : 'bruhendo'
+        }
+    },
+    methods : {
+        reverseMessage(){
+            this.message = this.message.split('').reverse().join('');
+        }
+    }
+}
 
+const userInput = {
+    data(){
+        return {
+            messages: ['bruhendo']
+        }
+    },
+    methods : {
+        palindrome(){
+            const theWord = this.messages[0].toLowerCase().split('').filter( item =>
+                item !== " "    
+            ).join('');
+            const theReversedWord = theWord.split('').reverse().join('');
+            if(theWord === theReversedWord){
+                this.messages.push("true")
+            }else{
+                this.messages.push("false")
+            }
+        }
+    }
+}
+
+Vue.createApp(userInput).mount('#twoWayDataBinding')
+Vue.createApp(handleTheEvent).mount('#event-handling');
 Vue.createApp(dating).mount('#setuped');
