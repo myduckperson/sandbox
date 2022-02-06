3f6b43c6882bfdb5a5abafc8c229a40b34d57ee2
myduckperson <illarionov0405@gmail.com>
2022-02-06 17:10:14 +0200

    the prop is finally understood

--- sandbox/main.js
+++ sandbox/main.js
@@ -1,4 +1,6 @@
 "strict mode"
+
+//  set the date
 const dating = {
     data(){
         return {
@@ -11,6 +13,12 @@ const dating = {
         }, 1000)
     }
 };
+
+
+
+
+
+// handle events plus some logic
 const handleTheEvent = {
     data(){
         return {
@@ -24,6 +32,10 @@ const handleTheEvent = {
     }
 }
 
+
+
+
+//  two way data binding plus some logic
 const userInput = {
     data(){
         return {
@@ -45,6 +57,49 @@ const userInput = {
     }
 }
 
+
+// whatever this is (components, but i am very confused)
+const apples = {
+    props: ['todo'],
+    template: `<li>{{todo.text}}</li>`
+};
+const oranges = {
+    data(){
+        return {
+            groceryList : [
+                { id:0, text: "veggies"},
+                { id:1, text: "meaties"},
+                { id:2, text: "humies"}
+            ]
+        }
+    },
+    components :{
+        apples
+    }
+}
+
+
+
+
+
+// a try to understand components
+const aplesBruh = {
+    template: `<li>This is a todo</li>`
+}
+
+// Create Vue application
+const app = Vue.createApp({
+    components: {
+        aplesBruh // Register a new component
+    }
+})
+
+// Mount Vue application
+app.mount('#attempt');
+
+
+
+Vue.createApp(oranges).mount("#debate");
 Vue.createApp(userInput).mount('#twoWayDataBinding')
 Vue.createApp(handleTheEvent).mount('#event-handling');
 Vue.createApp(dating).mount('#setuped');
